3a6fbeab31206f58e0348e0aad6d7bbb177ecb49
2drocket <simmobl@whs.school.nz>
2022-06-01 15:17:47 +1200

    Gui work

--- Djikstra/GuiMenu.java
+++ Djikstra/GuiMenu.java
@@ -8,16 +8,31 @@ import java.awt.*;
  */
 public class GuiMenu extends JFrame
 {
+    //Class variables
+    JMenuBar menuBar;
+    JMenu menu;
+    JMenuItem menuItem;
+    
     public GuiMenu()
     {
-        setTitle("Djikstra's Algorithm");
+        setTitle("Djikstra's Algorithm");//Gives the window a name
         
-        this.getContentPane().setPreferredSize(new Dimension(400,600));
+        this.getContentPane().setPreferredSize(new Dimension(400,600));//Sets the default size of the window
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Closes the window when you exit
         
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        menuBar=new JMenuBar(); //Creates the menu bar at the top
+        this.setJMenuBar(menuBar);
+        
+        menu = new JMenu("Settings"); //these lines of code create a variable which is a JMenu with a name and then add the menu to the bar
+        menuBar.add(menu);
+        menu = new JMenu("Help");
+        menuBar.add(menu);
+        
+        menuItem = new JMenuItem("Test");
+        menu.add(menuItem);
         
         this.pack();
         this.toFront();
-        this.setVisible(true);
+        this.setVisible(true);//Makes the window 
     }
 }
